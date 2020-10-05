c96d61f5edfe61b292e8d391c6f02049ea3f7d68
MateusKha <mateusgnascimento@outlook.com>
2020-10-05 00:15:15 -0300

    Project created

---
+++ backend/src/main/java/com/devsuperior/trabalhofinalcap1/Trabalhofinalcap1Application.java
@@ -0,0 +1,13 @@
+package com.devsuperior.trabalhofinalcap1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Trabalhofinalcap1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Trabalhofinalcap1Application.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/trabalhofinalcap1/Trabalhofinalcap1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.trabalhofinalcap1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Trabalhofinalcap1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
